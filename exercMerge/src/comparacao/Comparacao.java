eda8acc8ef9c0ed4b7d545a45e635a30c108eab5
Claudir Fantuci Junior <claudirfantucijunior@gmail.com>
2024-11-27 18:54:02 -0300

    27-11-2024

--- exercMerge/src/comparacao/Comparacao.java
+++ exercMerge/src/comparacao/Comparacao.java
@@ -5,40 +5,72 @@ import java.util.Random;
 
 public class Comparacao {
     public static void main(String[] args) {
-        int tamanho = 10000;
-        int[] vetor = new int[tamanho];
+        int[] tamanhos = { 10000, 100000, 1000000 };
+        int repeticoes = 10;
 
-        preencherVetorAleatorio(vetor);
+        for (int tamanhoAtual : tamanhos) {
+            long somaBubbleSort = 0;
+            long somaInsertionSort = 0;
+            long somaSelectionSort = 0;
+            long somaMergeSort = 0;
 
-        int[] vetorBubbleSort = Arrays.copyOf(vetor, vetor.length);
-        int[] vetorInsertionSort = Arrays.copyOf(vetor, vetor.length);
-        int[] vetorMergeSort = Arrays.copyOf(vetor, vetor.length);
-        int[] vetorSelectionSort = Arrays.copyOf(vetor, vetor.length);
+            int[] vetor;
+            int[] vetorBubbleSort;
+            int[] vetorInsertionSort;
+            int[] vetorMergeSort;
+            int[] vetorSelectionSort;
 
-         long inicioBubbleSort = System.currentTimeMillis();
-         Bolha.executarBubbleSort(vetorBubbleSort);
-         long fimBubbleSort = System.currentTimeMillis();
+            long inicioBubbleSort, fimBubbleSort, inicioInsertionSort, fimInsertionSort, inicioSelectionSort,
+                    fimSelectionSort, inicioMergeSort, fimMergeSort;
+            System.out.println("Tamanho do vetor: " + tamanhoAtual);
+            for (int i = 0; i < repeticoes; i++) {
+                vetor = new int[tamanhoAtual];
+                preencherVetorAleatorio(vetor);
 
-        long inicioInsertionSort = System.currentTimeMillis();
-        Insercao.executarInsertionSort(vetorInsertionSort);
-        long fimInsertionSort = System.currentTimeMillis();
+                vetorBubbleSort = Arrays.copyOf(vetor, vetor.length);
+                vetorInsertionSort = Arrays.copyOf(vetor, vetor.length);
+                vetorMergeSort = Arrays.copyOf(vetor, vetor.length);
+                vetorSelectionSort = Arrays.copyOf(vetor, vetor.length);
 
-         long inicioSelectionSort = System.currentTimeMillis();
-         Selecao.executarSelectionSort(vetorSelectionSort);
-         long fimSelectionSort = System.currentTimeMillis();
+                inicioBubbleSort = System.currentTimeMillis();
+                Bolha.executarBubbleSort(vetorBubbleSort);
+                fimBubbleSort = System.currentTimeMillis();
+                somaBubbleSort += (fimBubbleSort - inicioBubbleSort);
+                System.out.println("Tempo de execução do Bubble Sort execução " + i + " : " +
+                        (fimBubbleSort - inicioBubbleSort) / 1000.0 + " s");
 
-        long inicioMergeSort = System.currentTimeMillis();
-        Merge.executarMergeSort(vetorMergeSort);
-        long fimMergeSort = System.currentTimeMillis();
+                inicioInsertionSort = System.currentTimeMillis();
+                Insercao.executarInsertionSort(vetorInsertionSort);
+                fimInsertionSort = System.currentTimeMillis();
+                somaInsertionSort += (fimInsertionSort - inicioInsertionSort);
+                System.out.println("Tempo de execução do Insertion Sort execução " + i + " : " +
+                        (fimInsertionSort - inicioInsertionSort) / 1000.0 + " s");
 
-        System.out.println("Tempo de execução do Bubble Sort: " + 
-                String.format("%.4f", (fimBubbleSort - inicioBubbleSort) / 1000.0) + " s");
-        System.out.println("Tempo de execução do Insertion Sort: " + 
-                String.format("%.4f", (fimInsertionSort - inicioInsertionSort) / 1000.0) + " s");
-        System.out.println("Tempo de execução do Selection Sort: " + 
-                String.format("%.4f", (fimSelectionSort - inicioSelectionSort) / 1000.0) + " s");
-        System.out.println("Tempo de execução do Merge Sort: " + 
-                String.format("%.7f", (fimMergeSort - inicioMergeSort) / 1000.0) + " s");
+                inicioSelectionSort = System.currentTimeMillis();
+                Selecao.executarSelectionSort(vetorSelectionSort);
+                fimSelectionSort = System.currentTimeMillis();
+                somaSelectionSort += (fimSelectionSort - inicioSelectionSort);
+                System.out.println("Tempo de execução do Selection Sort execução " + i + " : " +
+                        (fimSelectionSort - inicioSelectionSort) / 1000.0 + " s");
+
+                inicioMergeSort = System.currentTimeMillis();
+                Merge.executarMergeSort(vetorMergeSort);
+                fimMergeSort = System.currentTimeMillis();
+                somaMergeSort += (fimMergeSort - inicioMergeSort);
+                System.out.println("Tempo de execução do Merge Sort execução " + i + " : " +
+                        (fimMergeSort - inicioMergeSort) / 1000.0 + " s");
+            }
+
+            System.out.println("Tamanho do vetor: " + tamanhoAtual);
+            System.out.println("Média do tempo de execução do Bubble Sort: " +
+                    String.format("%.4f", (somaBubbleSort / (double) repeticoes) / 1000.0) + " s");
+            System.out.println("Média do tempo de execução do Insertion Sort: " +
+                    String.format("%.4f", (somaInsertionSort / (double) repeticoes) / 1000.0) + " s");
+            System.out.println("Média do tempo de execução do Selection Sort: " +
+                    String.format("%.4f", (somaSelectionSort / (double) repeticoes) / 1000.0) + " s");
+            System.out.println("Média do tempo de execução do Merge Sort: " +
+                    String.format("%.7f", (somaMergeSort / (double) repeticoes) / 1000.0) + " s");
+        }
     }
 
     public static void preencherVetorAleatorio(int[] vetor) {
